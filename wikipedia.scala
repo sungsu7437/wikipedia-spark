a683c9732baf0f743abb682fe7d6581cb9c96273
손성수 <sungsu7437@naver.com>
2019-06-18 01:37:58 +0900

    comment added

--- wikipedia.scala
+++ wikipedia.scala
@@ -17,15 +17,38 @@ val langs = List(
 "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
 
 
-
+/** Problem 0:
+*  Returns the number of articles on which the language `lang` occurs.
+*  Hint: consider using method `mentionsLanguage` on `WikipediaArticle`
+*/
 def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int = ??
 
+
+/* Problem 1: Use `occurrencesOfLang` to compute the ranking of the languages
+ *     (`val langs`) by determining the number of Wikipedia articles that
+ *     mention each language at least once. Don't forget to sort the
+ *     languages by their occurrence, in decreasing order!
+ */
 def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ??
 
+
+/* Problem 2:
+ * Compute an inverted index of the set of articles, mapping each language
+ * to the Wikipedia pages in which it occurs.
+ */
 def makeIndex(langs: List[String], rdd: RDD[WikipediaArticle]): RDD[(String, Iterable[WikipediaArticle])] = ??
 
+
+/* Problem 3: Compute the language ranking again, but now using the inverted index. Can you notice
+ *     a performance improvement?
+ */
 def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = ??
 
+
+/* Problem 4: Use `reduceByKey` so that the computation of the index and the ranking are combined.
+ *     Can you notice an improvement in performance compared to measuring *both* the computation of the index
+ *     and the computation of the ranking?
+ */
 def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ??
 
 
